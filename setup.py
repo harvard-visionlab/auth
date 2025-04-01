0c9e282746a2d061c19c3edc2e4ab8e17529cebc
grez72 <grez72@gmail.com>
2025-04-01 16:41:06 +0000

    fixed install

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
-    name='visionlab.auth',
+    name='visionlab-auth',
     version='0.1.0',
     packages=['visionlab.auth'],
     package_dir={'visionlab.auth': 'auth'},
-    namespace_packages=['visionlab'],
     python_requires='>=3.3',
-    # Additional metadata (classifiers, author info, etc.)
+    # Other metadata such as classifiers, description, etc.
 )
